91caecf38622dc8ad12748873b6605442a1cbbcd
seymore <644827059@qq.com>
2021-12-15 15:42:55 +0800

    npm i dotenv && 打开CORS

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -1,8 +1,11 @@
 import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
+import { config as envInt } from 'dotenv';
 
 export type DefaultConfig = PowerPartial<EggAppConfig>;
 
 export default (appInfo: EggAppInfo) => {
+  envInt();
+
   const config = {} as DefaultConfig;
 
   // use for cookie sign key, should change to your own and keep security
@@ -17,9 +20,16 @@ export default (appInfo: EggAppInfo) => {
     replaceEggLogger: true,
   };
 
-  // config.security = {
-  //   csrf: false,
-  // };
+  // https://eggjs.org/zh-cn/core/security.html
+  config.security = {
+    // 配合egg-cors使用
+    // domainWhiteList: [ 'http://localhost:7003' ],
+    csrf: {
+      // 默认为 false，当设置为 true 时，将会放过所有 content-type 为 `application/json` 的请求
+      enable: false,
+      ignoreJSON: true,
+    },
+  };
 
   return config;
 };
