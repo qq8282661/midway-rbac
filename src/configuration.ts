819a986a3d001098f755787904c102cd42ae552a
seymore <644827059@qq.com>
2021-12-22 14:12:33 +0800

    axios

--- src/configuration.ts
+++ src/configuration.ts
@@ -5,9 +5,10 @@ import { ILifeCycle } from '@midwayjs/core';
 import { Application } from 'egg';
 import * as orm from '@midwayjs/orm';
 import * as redis from '@midwayjs/redis';
+import * as axios from '@midwayjs/axios';
 
 @Configuration({
-  imports: [orm, redis],
+  imports: [orm, redis, axios],
   importConfigs: [join(__dirname, './config')],
   conflictCheck: true,
 })
